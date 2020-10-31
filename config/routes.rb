2fd7c129df252f2d0d05ac960dbaddba7cdc2846
高津亘宏 <takatsunobuhiro@mbp.local>
2020-10-31 09:32:35 +0900

    sessiont_destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'toppages#home'
   devise_for :clubs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :club do
+    get '/clubs/sign_out' => 'devise/sessions#destroy'
+  end
 end
