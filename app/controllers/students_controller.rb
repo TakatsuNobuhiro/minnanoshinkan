deed2267649a78e1b20b7b8617a471e6ff09ef5e
高津亘宏 <takatsunobuhiro@mbp.local>
2020-11-03 21:56:30 +0900

    student profile view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,32 @@
 class StudentsController < ApplicationController
+  before_action :set_student ,only: [:show, :edit, :update]
   def index
-    @stundet=Stundet.all
+    @stundents = Student.all
   end
 
   def show
-    @student = Stundent.find(params[:id])
   end
 
   def edit
-    @student = Stundent.find(params[:id])
+  end
+
+  def update
+    if @studnet.update(student_params)
+      flash[:success]= 'プロフィールを編集しました'
+      redirect_to @student
+
+    else 
+      flash.now[:danger] = 'プロフィールの編集に失敗しました'
+      render :edit 
+    end
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name,:detail,:age,:universituy,:department,:hobby)
   end
 end
