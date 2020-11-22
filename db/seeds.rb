752234fe8ecf15fcd179e1be2215d93afab55f67
高津亘宏 <nobuhiro19980622@gmail.com>
2020-11-22 13:02:03 +0900

    seed 作成 #14

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+I18n.locale = :ja
+Faker::Config.locale = :en
+15.times do
+  student =
+    Student.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'hugahuga',
+      gender: 1,
+      university: '東京工業大学',
+      highschool: '広尾学園',
+      detail: '私は' + Faker::Food.dish + 'が好きです。'
+    )
+  student.avatar.attach(
+    io: File.open(Rails.root.join('db/seed_images/avatar.jpeg')),
+    filename: 'avatar.jpeg'
+  )
+end
+
+15.times do
+  club =
+    Student.create(
+      name: 'fake',
+      email: Faker::Internet.email,
+      password: 'hugahuga',
+      intercollege: true,
+      university: '東京工業大学',
+      active: true,
+      detail: '毎週火曜日にやってます。'
+    )
+  club.avatar.attach(
+    io: File.open(Rails.root.join('db/seed_images/tech.jpeg')),
+    filename: 'tech.jpeg'
+  )
+end
