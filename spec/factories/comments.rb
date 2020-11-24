a674426e4babe5839990c319d97d650dd77e7a5b
高津亘宏 <nobuhiro19980622@gmail.com>
2020-11-24 18:16:25 +0900

    comment rspec #18

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
     sequence(:content) { |n| "TEST_COMMENT#{n}" }
+    association :event
+    association :student
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe Comment, type: :model do
 
   describe 'validation' do
     it 'そのままならOK' do
-      # expect(@comment.valid?).to eq(true)
+      expect(@comment.valid?).to eq(true)
+    end
+    it 'commentが141文字ならアウト' do
+      @comment.content = 'a' * 141
+      expect(@comment.valid?).to eq(false)
+    end
+    it 'commentが140文字ならOK' do
+      @comment.content = 'a' * 140
+      expect(@comment.valid?).to eq(true)
     end
   end
 end
